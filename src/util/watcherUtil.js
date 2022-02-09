fee0d83d253837fd0ce64b5e3e9c8022c5f2ac96
Ryan <ryanpricelondon@gmail.com>
2022-02-09 16:33:07 -0500

    changed RPC back to finality

--- src/util/watcherUtil.js
+++ src/util/watcherUtil.js
@@ -12,12 +12,14 @@ const networkId = isLocal ? 1287 : 1285;
 const zoomTokenContractAddress = zoomTokenContractJson.networks[networkId].address;
 const zoombiesContractAddress = zoombiesContractJson.networks[networkId].address;
 
-const providerURL = isLocal ? "https://rpc.api.moonbase.moonbeam.network" : "https://rpc.api.moonriver.moonbeam.network";
+//const providerURL = isLocal ? "https://rpc.api.moonbase.moonbeam.network" : "https://rpc.api.moonriver.moonbeam.network";
+const providerURL = isLocal ? "https://moonbeam-alpha.api.onfinality.io/public" : "https://moonriver.api.onfinality.io/public";
 // Define Provider
 const rpcProvider = new ethers.providers.StaticJsonRpcProvider(providerURL, {
   chainId: networkId,
   name: isLocal ? "moonbase-alphanet" : "Moonriver",
 });
+
 
 export const ZoomContract = new ethers.Contract(
   zoomTokenContractAddress,
